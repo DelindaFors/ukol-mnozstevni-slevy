9d851c4c4dfc3f7305ed925e90479024e03f1cb0
Daniela Ticha <daniela.ticha@post.cz>
2023-11-02 14:54:04 +0100

    Byl pridan index.html a script.js

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
+const pocetKs = prompt ("Zadejte daný počet ks k objednání:")
 
-
+if (pocetKs < 50) {
+    document.body.innerHTML = pocetKs * 300 + " Kč"
+} else if (pocetKs <= 200) {
+    document.body.innerHTML = pocetKs * 250 + " Kč"
+} else if (pocetKs <= 500) {
+    document.body.innerHTML = pocetKs * 200 + " Kč"
+} else if (pocetKs <= 1000){
+    document.body.innerHTML = pocetKs * 150 + " Kč"
+} else if (pocetKs > 1001){
+    document.body.innerHTML = pocetKs * 120 + " Kč"
+}
